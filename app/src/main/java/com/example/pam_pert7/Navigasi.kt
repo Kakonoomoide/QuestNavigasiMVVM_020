2ab1d7213800d10df31c6fd553aa768f92058a5b
Kakonoomoide <rizkiramada128@gmail.com>
2024-11-20 22:13:05 +0700

    ADD - Menambahkan parameter modifier, viewModel, dan navHost

--- app/src/main/java/com/example/pam_pert7/Navigasi.kt
+++ app/src/main/java/com/example/pam_pert7/Navigasi.kt
@@ -1,6 +1,12 @@
 package com.example.pam_pert7
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHost
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.pam_pert7.ui.viewmodel.MahasiswaViewModel
 
 enum class Halaman{
     Form,
@@ -9,6 +15,9 @@ enum class Halaman{
 
 @Composable
 fun Navigasi(
+    modifier: Modifier = Modifier,
+    viewModel: MahasiswaViewModel = viewModel(),
+    navHost: NavHostController = rememberNavController()
 ){
 
 }
